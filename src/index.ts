b8b80fab65f80db032a6563491be07951d1111aa
Curslo <richardkisivii@gmail.com>
2023-04-12 15:58:37 +0300

    Views

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,20 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (req, res) => res.send("Welcome to my Frontend"))
+app.set("view engine", "ejs");
 
-const PORT = 8080;
+//route for index page
+app.get("/", function (req, res) {
+    res.render("index");
+  });
+  
+  //route for magic page
+  app.get("/magic", function (req, res) {
+    res.render("magic");
+  });
+  
+
+const PORT = 7000;
 
 app.listen(PORT, () => 
 console.log(`Server Running on port: http://localhost:${PORT}`));
